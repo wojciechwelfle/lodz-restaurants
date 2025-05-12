1e562efbc5fbf8cd1eaf94182df9c427b98c000f
Wafelek <111795716+wojciechwelfle@users.noreply.github.com>
2025-05-12 16:38:49 +0200

    Init project structure

---
+++ backend/lodz-restaurants/src/main/java/com/lodzrestaurants/lodzrestaurants/LodzRestaurantsApplication.java
@@ -0,0 +1,13 @@
+package com.lodzrestaurants.lodzrestaurants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LodzRestaurantsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LodzRestaurantsApplication.class, args);
+    }
+
+}
---
+++ backend/lodz-restaurants/src/test/java/com/lodzrestaurants/lodzrestaurants/LodzRestaurantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lodzrestaurants.lodzrestaurants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LodzRestaurantsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
