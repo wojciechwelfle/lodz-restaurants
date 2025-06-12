1f09d914c8000f6ab3a8e572f3a312e70859bf00
Wafelek <111795716+wojciechwelfle@users.noreply.github.com>
2025-06-12 22:46:43 +0200

    Clean up

--- backend/lodz-restaurants/src/main/java/com/lodzrestaurants/lodzrestaurants/controller/MenuApi.java
+++ backend/lodz-restaurants/src/main/java/com/lodzrestaurants/lodzrestaurants/controller/MenuApi.java
@@ -2,22 +2,19 @@ package com.lodzrestaurants.lodzrestaurants.controller;
 
 import com.lodzrestaurants.lodzrestaurants.dataaccess.dto.MenuDto;
 import com.lodzrestaurants.lodzrestaurants.service.MenuService;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/v1/menu")
@@ -32,11 +29,13 @@ public class MenuApi {
         this.menuService = menuService;
     }
 
+    @Schema(name = "Get menus", description = "Get all menus")
     @GetMapping
     public ResponseEntity<List<MenuDto>> getAllMenus() {
         return ResponseEntity.ok(menuService.getAllMenus());
     }
 
+    @Schema(name = "Get menu by ID", description = "Get a specific menu by its ID")
     @GetMapping("/{menuId}")
     public ResponseEntity<MenuDto> getMenu(@PathVariable Long menuId) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -47,19 +46,5 @@ public class MenuApi {
         return ResponseEntity.ok(menuService.getMenu(menuId, isAuthenticated));
     }
 
-    @GetMapping("/auth-check")
-    public ResponseEntity<Map<String, Object>> checkAuth() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Map<String, Object> result = new HashMap<>();
-        
-        result.put("isAuthenticated", authentication != null && authentication.isAuthenticated());
-        result.put("principal", authentication != null ? authentication.getPrincipal() : null);
-        result.put("authorities", authentication != null ? authentication.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.toList()) : null);
-        result.put("details", authentication != null ? authentication.getDetails() : null);
-        
-        return ResponseEntity.ok(result);
-    }
 
 }
